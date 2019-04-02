aca14df45477f8b8f0f7c9ed4d7bd1751338af67
scrain92 <crain3@illinois.edu>
2019-04-02 07:29:58 -0700

    practicing quick sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -6,7 +6,44 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Have fun practicing sorting algorithms!");
+
+            var numbers = new int[] { 3,7,8,5,2,1,9,5,4 };
+            Console.WriteLine("Initial Order:");
+            Console.WriteLine(String.Join(", ", numbers));
+            
+            QuickSort(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine("Final order:");
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+
+        static void QuickSort(int[] array, int lo, int hi) {
+            if (lo < hi) {
+                var p = Partition(array, lo, hi);
+                QuickSort(array, lo, p - 1);
+                QuickSort(array, p + 1, hi);
+            }
+        }
+
+        static int Partition (int[] array, int lo, int hi) {
+            var pivot = array[hi];
+            var i = lo;
+
+            for(int j = lo; j < hi - 1; j++) {
+                if (array[j] < pivot) {
+                    var t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                    i++;
+                }
+            }
+
+            var temp = array[i];
+            array[i] = array[hi];
+            array[hi] = temp;
+
+            return i;
         }
     }
 }
